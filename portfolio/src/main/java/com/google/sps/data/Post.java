8fd9a2260faa7ec73f005aa3d2328a62b1177e43
aamanhey <amanhey@googl.com>
2020-06-17 18:05:04 +0000

    Deletes unused code in Post.java

--- portfolio/src/main/java/com/google/sps/data/Post.java
+++ portfolio/src/main/java/com/google/sps/data/Post.java
@@ -8,10 +8,6 @@ public final class Post {
   private final String name;
   private final String text;
   private final long timestamp;
-//   private final String bucketName;
-//   private final String objectName;
-//   private final BlobId imgObjId;
-//   private final Blob imgfile;
   private final String imgUrl;
 
   public Post(long id, String name, String text, long timestamp, String imgUrl) {
@@ -21,16 +17,4 @@ public final class Post {
       this.timestamp = timestamp;
       this.imgUrl = imgUrl;
     }
-
-//   public Post(long id, String name, String text, long timestamp, String bucketName,
-//       String objectName, BlobId imgObjId, Blob imgfile) {
-//     this.id = id;
-//     this.name = name;
-//     this.text = text;
-//     this.timestamp = timestamp;
-//     this.bucketName = bucketName;
-//     this.objectName = objectName;
-//     this.imgObjId = imgObjId;
-//     this.imgfile = imgfile;
-//   }
 }
