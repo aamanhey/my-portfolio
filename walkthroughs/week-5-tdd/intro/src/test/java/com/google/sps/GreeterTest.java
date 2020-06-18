3c82ced069cc9b6492420b75420f83830b6d85fa
aamanhey <amanhey@googl.com>
2020-06-18 01:53:52 +0000

    Modifies Greeter.java to remove whitespace and @#$% characters and test the method.

--- walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
+++ walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
@@ -11,23 +11,32 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps;
-
+ 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
+ 
 @RunWith(JUnit4.class)
 public final class GreeterTest {
-
+ 
   @Test
   public void testGreeting() {
     Greeter greeter = new Greeter();
-
+ 
     String greeting = greeter.greet("Ada");
-
+ 
+    Assert.assertEquals("Hello Ada", greeting);
+  }
+  @Test
+  public void testGreetingTrimsWhitespace() {
+    Greeter greeter = new Greeter();
+ 
+    String greeting = greeter.greet("   Ada   ");
+ 
+    // Whitespace should be trimmed
     Assert.assertEquals("Hello Ada", greeting);
   }
 }
